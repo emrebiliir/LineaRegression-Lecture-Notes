bf6e8759cac30f56b4486713a6fd7b0e95732df3
Emre Bilir <117826843+emrebiliir@users.noreply.github.com>
2023-06-13 11:14:51 +0300

    Update Linear-Regression.py

--- Linear-Regression.py
+++ Linear-Regression.py
@@ -1,10 +1,3 @@
-######################################################
-# Sales Prediction with Linear Regression
-######################################################
-
-# I have developed a sales forecasting model. This model indicates the amount of sales obtained as a result of advertising expenses made on various channels.
-# My first task is to build a simple regression model with two variables, and then proceed to build a model with all five variables present in the dataset.
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
